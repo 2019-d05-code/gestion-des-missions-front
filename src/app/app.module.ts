71da7bf211cc8d7b10150011475d398f39c922c9
BIRABEN-BIANCHI Hugo <birabenbianchihugo@gmail.com>
2019-05-21 14:55:14 +0200

    Merge pull request #26 from 2019-d05-code/interface-ajout-missions-tda
    Interface ajout missions tda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,52 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { TechComponent } from './tech/tech.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AuthComponent } from './auth/auth.component';
-import {FormsModule} from "@angular/forms";
-import {StatutConnecteService} from "./auth/statut-connecte.service";
-import {AuthInterceptorService} from "./auth/auth-interceptor.service";
-import { DateDebutValidateurDirective } from './validateurs/date-debut-validateur.directive';
-import { FormatDateValidateurDirective } from './validateurs/format-date-validateur.directive';
+import { FormsModule } from '@angular/forms';
+import { StatutConnecteService } from './auth/statut-connecte.service';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { DateFinValidateurDirective } from './validateurs/date-fin-validateur.directive';
-import { FormulaireTestComponent } from './test/formulaire-test/formulaire-test.component';
+import { FormatDateValidateurDirective } from './validateurs/format-date-validateur.directive';
+import { DateDebutValidateurDirective } from './validateurs/date-debut-validateur.directive';
 import { CreationMissionComponent } from './creation-mission/creation-mission.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { ErreurComponent } from './erreur/erreur.component';
 
 const routes: Routes = [
-  { path: 'tech', component: TechComponent, canActivate: [StatutConnecteService] }, // /tech accessible uniquement si connecté
-  { path: 'auth', component: AuthComponent },
-  { path: '', component: CreationMissionComponent }
- // { path: '', redirectTo: '/tech', pathMatch: 'full' },
+    { path: 'tech', component: TechComponent, canActivate: [StatutConnecteService] }, // /tech accessible uniquement si connecté
+    { path: 'auth', component: AuthComponent },
+    { path: 'creation-mission', component: CreationMissionComponent },
+    { path: 'connexion', component: ConnexionComponent },
+    { path: 'erreur', component: ErreurComponent },
+    { path: '', redirectTo: '/tech', pathMatch: 'full' }
 ];
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TechComponent,
-    AuthComponent,
-    DateDebutValidateurDirective,
-    FormatDateValidateurDirective,
-    DateFinValidateurDirective,
-    FormulaireTestComponent
-    CreationMissionComponent
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(routes),
-    HttpClientModule,
-    MDBBootstrapModule.forRoot(),
-    FormsModule
-  ],
-  providers: [{
-    provide: HTTP_INTERCEPTORS,
-    useClass: AuthInterceptorService,
-    multi: true
-  }],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TechComponent,
+        AuthComponent,
+        CreationMissionComponent,
+        ConnexionComponent,
+        ErreurComponent,
+        DateFinValidateurDirective,
+        FormatDateValidateurDirective,
+        DateDebutValidateurDirective,
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(routes),
+        HttpClientModule,
+        MDBBootstrapModule.forRoot(),
+        FormsModule
+    ],
+    providers: [{
+        provide: HTTP_INTERCEPTORS,
+        useClass: AuthInterceptorService,
+        multi: true
+    }],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
