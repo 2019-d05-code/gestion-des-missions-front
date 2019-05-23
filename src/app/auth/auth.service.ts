f969ed7032e4e212840cb660f180f539bacaa504
BIRABEN-BIANCHI Hugo <birabenbianchihugo@gmail.com>
2019-05-23 13:44:32 +0200

    Correction Jenkins

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,8 +5,7 @@ import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs/internal/Observable';
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 import { of } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
-import { StatutConnecteService } from './statut-connecte.service';
+import { catchError, map, tap } from 'rxjs/operators'; è
 
 /**
  * Collègue anonyme.
