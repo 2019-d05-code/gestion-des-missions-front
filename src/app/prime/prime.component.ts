d68c8fb87fd22e966671d7243d4bd3278bfbb31d
Genie <gspaul6@gmail.com>
2019-05-29 19:02:08 +0200

    prime just begening

---
+++ src/app/prime/prime.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prime',
+  template: `
+    <p>
+      prime works!
+    </p>
+  `,
+  styles: []
+})
+export class PrimeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
