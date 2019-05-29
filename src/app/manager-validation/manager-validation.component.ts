54f6d1d9d1fbfbc2102fc06dcb99a81dfb2a927b
BIRABEN-BIANCHI Hugo <birabenbianchihugo@gmail.com>
2019-05-29 10:21:53 +0200

    Suppression Alertes JS + ajout titres + changement manager validation

--- src/app/manager-validation/manager-validation.component.ts
+++ src/app/manager-validation/manager-validation.component.ts
@@ -4,52 +4,47 @@ import { Mission, MissionManager, MissionStatut } from '../modeles/Mission';
 import { Statut } from '../modeles/Statut';
 
 @Component({
-  selector: 'app-manager-validation',
-  templateUrl: './manager-validation.component.html',
-  styles: []
+    selector: 'app-manager-validation',
+    templateUrl: './manager-validation.component.html',
+    styles: []
 })
-export class ManagerValidationComponent implements OnInit
-{
+export class ManagerValidationComponent implements OnInit {
     listeMission: MissionManager[];
     miss: MissionStatut = new MissionStatut(null, null);
     statut: any = {};
 
-  constructor(private _serv:DataService) { }
+    constructor(private _serv: DataService) { }
 
-  ngOnInit()
-  {
-    this.afficherListe ();
-  }
+    ngOnInit() {
+        this.afficherListe();
+    }
 
-  afficherListe ()
-  {
-    this._serv.recupererMissionManager()
-    .subscribe( coll => {this.listeMission = coll; },
-        (error: Error) => { alert(`${error.name} : ${error.message}`); } );
-  }
+    afficherListe() {
+        this._serv.recupererMissionManager()
+            .subscribe(coll => { this.listeMission = coll; },
+                (error: Error) => { });
+    }
 
-  valider(i: number)
-  {
-    // traitement du patch
-    this.miss.id = this.listeMission[i].id;
-    this.miss.statut = 2;
-    this._serv.changerStatutMission(this.miss).subscribe( miss => {
-        alert('envoyer');
-        this.afficherListe (); },
-        (error: Error) => { alert(`${error.name} : ${error.message}`); } ,
-        () => {}  );
-  }
+    valider(i: number) {
+        // traitement du patch
+        this.miss.id = this.listeMission[i].id;
+        this.miss.statut = 2;
+        this._serv.changerStatutMission(this.miss).subscribe(miss => {
+            this.afficherListe();
+        },
+            (error: Error) => { },
+            () => { });
+    }
 
-  refuser(i: number)
-  {
-    // traitement du patch
-    this.miss.id = this.listeMission[i].id;
-    this.miss.statut = 3;
-    this._serv.changerStatutMission(this.miss).subscribe( miss => {
-        this.afficherListe ();
-        alert('envoyer'); },
-        (error: Error) => { alert(`${error.name} : ${error.message}`); } ,
-        () => {}  );
-  }
+    refuser(i: number) {
+        // traitement du patch
+        this.miss.id = this.listeMission[i].id;
+        this.miss.statut = 3;
+        this._serv.changerStatutMission(this.miss).subscribe(miss => {
+            this.afficherListe();
+        },
+            (error: Error) => { },
+            () => { });
+    }
 
 }
