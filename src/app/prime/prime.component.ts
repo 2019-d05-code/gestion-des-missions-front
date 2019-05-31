d527c58ade4b12e31d596f2cc762cb61520c958c
Genie <gspaul6@gmail.com>
2019-05-31 09:57:24 +0200

    resolve conflit

--- src/app/prime/prime.component.ts
+++ src/app/prime/prime.component.ts
@@ -8,11 +8,12 @@ import { Collegue } from '../auth/auth.domains';
 import { AuthService } from '../auth/auth.service';
 import { CollConn } from '../modeles/Collaborateur';
 import { stringify } from '@angular/compiler/src/util';
+import { PrimeService } from '../services/prime.service';
 
 @Component({
-  selector: 'app-prime',
-  templateUrl: `./prime.component.html`,
-  styleUrls: [`./prime.component.css`]
+    selector: 'app-prime',
+    templateUrl: `./prime.component.html`,
+    styleUrls: [`./prime.component.css`]
 })
 export class PrimeComponent implements OnInit {
     messageOk: string;
@@ -22,56 +23,56 @@ export class PrimeComponent implements OnInit {
     listeMissionDto: MissionDto[];
     constructor(private service: PrimeService, private router: Router) { }
 
-  ngOnInit() {
-      this.updateMission();
-  }
+    ngOnInit() {
+        this.updateMission();
+    }
 
-  updateMission(): void {
-    this.service.recupererListeMissionsDto().subscribe( coll => {
-        this.listeMissionDto = coll;
-    });
-}
-  trierMissionDateDebutAsc() {
-    this.listeMissionDto.sort(
-        (missiona: MissionDto, missionb: MissionDto) => {
-            return (new Date(missiona.dateDebut).getTime() - new Date(missionb.dateDebut).getTime());
-        }
-    );
-    this.tri('dateDebutAsc');
-    return this.listeMissionDto;
-}
+    updateMission(): void {
+        this.service.recupererListeMissionsDto().subscribe(coll => {
+            this.listeMissionDto = coll;
+        });
+    }
+    trierMissionDateDebutAsc() {
+        this.listeMissionDto.sort(
+            (missiona: MissionDto, missionb: MissionDto) => {
+                return (new Date(missiona.dateDebut).getTime() - new Date(missionb.dateDebut).getTime());
+            }
+        );
+        this.tri('dateDebutAsc');
+        return this.listeMissionDto;
+    }
 
-trierMissionDateFinAsc() {
-    this.listeMissionDto.sort(
-        (missiona: MissionDto, missionb: MissionDto) => {
-            return (new Date(missiona.dateFin).getTime() - new Date(missionb.dateFin).getTime());
-        }
-    );
-    this.tri('dateFinAsc');
-    return this.listeMissionDto;
-}
+    trierMissionDateFinAsc() {
+        this.listeMissionDto.sort(
+            (missiona: MissionDto, missionb: MissionDto) => {
+                return (new Date(missiona.dateFin).getTime() - new Date(missionb.dateFin).getTime());
+            }
+        );
+        this.tri('dateFinAsc');
+        return this.listeMissionDto;
+    }
 
-trierMissionDateDebutDesc() {
-    this.listeMissionDto.sort(
-        (missiona: MissionDto, missionb: MissionDto) => {
-            return (new Date(missionb.dateDebut).getTime() - new Date(missiona.dateDebut).getTime());
-        }
-    );
-    this.tri('dateDebutDesc');
-    return this.listeMissionDto;
-}
+    trierMissionDateDebutDesc() {
+        this.listeMissionDto.sort(
+            (missiona: MissionDto, missionb: MissionDto) => {
+                return (new Date(missionb.dateDebut).getTime() - new Date(missiona.dateDebut).getTime());
+            }
+        );
+        this.tri('dateDebutDesc');
+        return this.listeMissionDto;
+    }
 
-trierMissionDateFinDesc() {
-    this.listeMissionDto.sort(
-        (missiona: MissionDto, missionb: MissionDto) => {
-            return (new Date(missionb.dateFin).getTime() - new Date(missiona.dateFin).getTime());
-        }
-    );
-    this.tri('dateFinDesc');
-    return this.listeMissionDto;
-}
+    trierMissionDateFinDesc() {
+        this.listeMissionDto.sort(
+            (missiona: MissionDto, missionb: MissionDto) => {
+                return (new Date(missionb.dateFin).getTime() - new Date(missiona.dateFin).getTime());
+            }
+        );
+        this.tri('dateFinDesc');
+        return this.listeMissionDto;
+    }
 
-tri(valeur: string) {
-    this.trierPar = valeur;
-}
+    tri(valeur: string) {
+        this.trierPar = valeur;
+    }
 }
